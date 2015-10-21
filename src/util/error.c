3625cbe241efa63ebefd747159ac62e906af0859
annu13 <annapurna.dasari@intel.com>
2015-10-21 13:23:44 -0700

    add logic to select error handler based on error and info keys

--- src/util/error.c
+++ src/util/error.c
@@ -35,6 +35,7 @@
 
 #include "src/util/error.h"
 #include "src/include/pmix_globals.h"
+#include "src/buffer_ops/buffer_ops.h"
 
 const char* PMIx_Error_string(pmix_status_t errnum)
 {
@@ -122,11 +123,25 @@ const char* PMIx_Error_string(pmix_status_t errnum)
         return "INVALID-CREDENTIAL";
     case PMIX_EXISTS:
         return "EXISTS";
-
+    case PMIX_ERR_SERVER_FAILED_REQUEST:
+        return "SERVER FAILED REQUEST";
+    case PMIX_ERR_PROC_MIGRATE:
+        return "PROC-MIGRATE";
+    case PMIX_ERR_PROC_CHECKPOINT:
+        return "PROC-CHECKPOINT-ERROR";
+    case PMIX_ERR_PROC_RESTART:
+        return "PROC_RESTART";
+    case PMIX_ERR_PROC_ABORTING:
+        return "PROC-ABORTING";
+    case PMIX_ERR_PROC_REQUESTED_ABORT:
+        return "PROC-ABORT-REQUESTED";
+    case PMIX_ERR_PROC_ABORTED:
+        return "PROC-ABORTED";
     case PMIX_ERROR:
         return "ERROR";
     case PMIX_SUCCESS:
         return "SUCCESS";
+
     }
     return "ERROR STRING NOT FOUND";
 }
@@ -136,10 +151,49 @@ void pmix_errhandler_invoke(pmix_status_t status,
                             pmix_info_t info[], size_t ninfo)
 {
     /* We need to parse thru each registered handler and determine
-       which one to call for that error */
-    /*if (NULL != pmix_globals.errregs[0].errhandler) {
-        pmix_globals.errregs[0].errhandler(status, procs, nprocs, info, ninfo);
-    }*/
+       which one to call for the specific error */
+    int matched_errregs[PMIX_MAX_ERROR_REGISTRATIONS];
+    int nmatched = 0;
+    int i,j,k;
+    bool exact_match = false;
+    int allerrhandler_ind = -1;
+    pmix_error_reg_info_t *errreg;
+    for (i = 0; i < pmix_pointer_array_get_size(&pmix_globals.errregs) && !exact_match; i++) {
+        errreg = (pmix_error_reg_info_t*) pmix_pointer_array_get_item (&pmix_globals.errregs, i);
+        if(0 == errreg->ninfo) {
+            // this is a general err handler we will call it if there is no better match
+            allerrhandler_ind = i;
+        } else {
+            /* match error name key first */
+            for(j = 0; j < errreg->ninfo; j++) {
+                if ((0 == strcmp(errreg->info[j].key, PMIX_ERROR_NAME)) &&
+                        (status == errreg->info[j].value.data.int32)) {
+                     matched_errregs[0] = i;
+                     nmatched = 1;
+                     exact_match = true;
+                     break;
+                } else {
+                    for (k = 0; k < errreg->ninfo; k++) {
+                        if ((0 == strcmp(errreg->info[j].key, info[k].key)) &&
+                            (pmix_value_cmp(&errreg->info[j].value, &info[k].value))) {
+                            matched_errregs[nmatched++] = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for ( i =0 ;i < nmatched; i++) {
+        errreg = (pmix_error_reg_info_t*) pmix_pointer_array_get_item (&pmix_globals.errregs,
+                                                                        matched_errregs[i]);
+        errreg->errhandler (status, procs, nprocs, info, ninfo);
+    }
+    if ( 0 == nmatched && 0 <= allerrhandler_ind) {
+        errreg = (pmix_error_reg_info_t*) pmix_pointer_array_get_item (&pmix_globals.errregs,
+                  allerrhandler_ind);
+        errreg->errhandler (status, procs, nprocs, info, ninfo);
+    }
 }
 
 pmix_status_t pmix_lookup_errhandler(pmix_notification_fn_t err,
@@ -194,27 +248,29 @@ void pmix_get_errorgroup ( pmix_status_t status, char *pmix_error_group)
     switch(status) {
         case PMIX_ERR_UNREACH:
         case PMIX_ERR_COMM_FAILURE:
-        case PMIX_ERR_SERVER:
         case PMIX_ERR_SERVER_NOT_AVAIL:
-            strcpy(pmix_error_group, PMIX_ERR_GROUP_COMM_FAILURE);
+        case PMIX_ERR_TIMEOUT:
+        case PMIX_ERR_PACK_FAILURE:
+        case PMIX_ERR_UNPACK_FAILURE:
+            strcpy(pmix_error_group, PMIX_ERROR_GROUP_COMM_FAIL);
             break;
         case PMIX_ERR_OUT_OF_RESOURCE:
         case PMIX_ERR_RESOURCE_BUSY:
         case PMIX_ERR_NOMEM:
-            strcpy(pmix_error_group, PMIX_ERR_GROUP_RESOURCE);
+            strcpy(pmix_error_group, PMIX_ERROR_GROUP_RESOURCE);
             break;
-        case PMIX_ERR_PROC_MIGRATING:
-        case PMIX_ERR_PROC_CHECKPOINTING:
+        case PMIX_ERR_PROC_MIGRATE:
+        case PMIX_ERR_PROC_CHECKPOINT:
         case PMIX_ERR_PROC_RESTART:
-            strcpy(pmix_error_group, PMIX_ERR_GROUP_MIGRATION);
+            strcpy(pmix_error_group, PMIX_ERROR_GROUP_MIGRATE);
             break;
-        case PMIX_ERR_PROC_ABORT_IN_PROGRESS:
+        case PMIX_ERR_PROC_ABORTING:
         case PMIX_ERR_PROC_REQUESTED_ABORT:
         case PMIX_ERR_PROC_ABORTED:
-            strcpy(pmix_error_group, PMIX_ERR_GROUP_ABORT);
+            strcpy(pmix_error_group, PMIX_ERROR_GROUP_ABORT);
             break;
         default:
-            strcpy(pmix_error_group, PMIX_ERR_GROUP_GENERAL);
+            strcpy(pmix_error_group, PMIX_ERROR_GROUP_GENERAL);
     }
 }
 
